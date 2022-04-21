934e59d0967328ef2fa29b464b89160f9ca69a0a
palfirobert <palfi.robert14@yahoo.com>
2022-04-21 23:01:18 +0300

    Conectare firebase+ sign In/Log in

--- app/src/main/java/com/example/shuffset/MainActivity.java
+++ app/src/main/java/com/example/shuffset/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();//dasda
 
 
 
