4ccbbba9dfffb382593ec1ccd103cdd3923dc704
palfirobert <palfi.robert14@yahoo.com>
2022-04-21 22:48:15 +0300

    Conectare la Firebase

---
+++ app/src/main/java/com/example/shuffset/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.shuffset;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText emailEditText,passwordEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailEditText=findViewById(R.id.email);
+        passwordEditText=findViewById(R.id.password);
+
+        getSupportActionBar().hide();
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+    }
+
+
+
+
+    private static void hideSoftKeyboard(Activity activity) { // sa dispara tastatura cand dai in afara scrisului
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        if(inputMethodManager.isAcceptingText()){
+            inputMethodManager.hideSoftInputFromWindow(
+                    activity.getCurrentFocus().getWindowToken(),
+                    0
+            );
+        }
+    }
+    public void onClickBackground(View view)
+    {
+        hideSoftKeyboard(this);
+    }
+
+    public void SignIn(View view)
+    {
+        mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Sign Up was a success.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+
+                            Toast.makeText(getApplicationContext(), "Sign Up failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+    }
+
+    public void LogIn(View view)
+    {
+        mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            ;
+                        }
+                    }
+                });
+    }
+}
